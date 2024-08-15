8a8d237497a9708232e6bf1b589ac01ab669e467
Fonseca Claudio <fonsecaclaudioroman2000@gmail.com>
2024-08-15 11:51:49 -0300

    Se creo Header.jsx y Header.css

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,41 @@
+import "./Header.css";
+
+const Header = (props) => {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container-fluid">
+        <div className="social-icons">
+          <a href="https://github.com/ClaudioFonseca2002" target="_blank">
+            <FaGithub size={40} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/claudio-rom%C3%A1n-fonseca-551785313/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin size={32} />
+          </a>
+        </div>
+        <button type="button" className="btn btn-primary">
+          Ingresar
+        </button>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Contacto
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Proyectos
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
