f8db777db584d6e5f2c5483c28d0880359151d26
Fonseca Claudio <fonsecaclaudioroman2000@gmail.com>
2024-08-15 15:57:16 -0300

    Cambios en Login.jsx

--- src/views/login/Login.jsx
+++ src/views/login/Login.jsx
@@ -3,13 +3,16 @@ import styles from "./Login.module.css";
 //Importo dependencias
 import { Link } from "react-router-dom";
 import { useState } from "react";
-
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   //Estados
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
   const [error, setError] = useState(""); // Estado para manejar errores
+  const navigate = useNavigate();
+
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
   //Manejo de campos
   const handleUsername = (e) => {
@@ -23,11 +26,39 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError(""); // Limpiar errores anteriores
+
+    // Validar campos de entrada
+    if (!username || !password) {
+      setError("Por favor, ingresa tu nombre de usuario y contraseña.");
+      return;
+    }
+
     const data = {
       username,
       password,
     };
-    console.log("Datos enviado correctamente");
+
+    try {
+      const response = await fetch(`http://${backendUrl}/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message || `Error: ${response.statusText}`);
+      }
+
+      const result = await response.json();
+      localStorage.setItem("token", result.token);
+      navigate("/"); // Redirecciona a la vista "/"
+    } catch (error) {
+      console.error("Error al autenticar:", error);
+      setError(error.message || "Error al autenticar. Inténtalo de nuevo.");
+    }
   };
 
   return (
