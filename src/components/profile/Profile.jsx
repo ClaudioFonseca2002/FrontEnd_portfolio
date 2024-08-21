d515e2b9b71a047f1cecacb8612287c38c809493
Fonseca Claudio <fonsecaclaudioroman2000@gmail.com>
2024-08-20 23:00:49 -0300

    Se ha creado componente Profile.jsx

---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,52 @@
+import "./Profile.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import roman from "../../img/roman.jpeg";
+
+const Profile = () => {
+  //Estados
+  const [profileDescription, setProfileDescription] = useState([]);
+  const id_profile = 1;
+
+  //VariablesDeEntorno
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const LOCAL_BACKEND = import.meta.env.VITE_LOCAL_BACKEND;
+
+  //Traer descripcion del perfil desde la base de datos
+  useEffect(() => {
+    const fecthProfileDescription = async () => {
+      try {
+        const res = await axios.get(`${backendUrl}/profile`);
+        setProfileDescription(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fecthProfileDescription();
+  }, []);
+
+  //Vista componente
+
+  return (
+    <div className="container_perfil">
+      <div className="container_title">
+        <h2>Perfil</h2>
+      </div>
+      <div className="container_img_text">
+        <img src={roman} alt="Profile" className="img" />
+        <div className="description-container">
+          <ul>
+            {profileDescription.map((profile) => (
+              <li key={profile.id_perfil}>
+                <p className="description-text">{profile.perfil_descripcion}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,10 +1,12 @@
 import "./Home.css";
 import Header from "../../components/header/Header";
+import Profile from "../../components/profile/Profile.jsx"
 
 const Home = () => {
   return (
     <>
       <Header></Header>
+      <Profile></Profile>
     </>
   );
 };
