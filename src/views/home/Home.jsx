547a3176e128e8272cbb3a31649a9e390d2f77cd
Fonseca Claudio <fonsecaclaudioroman2000@gmail.com>
2024-08-25 19:00:11 -0300

    Se ha removido Status.jsx de Home.jsx

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -2,7 +2,6 @@ import "./Home.css";
 import Header from "../../components/header/Header";
 import Profile from "../../components/profile/Profile.jsx"
 import Proyect from "../../components/proyect/Proyect.jsx"
-import Status from "../../components/status/Status.jsx";
 import Education from "../../components/education/Education.jsx";
 import Technology from "../../components/technology/Technology.jsx";
 import Footer from "../../components/footer/Footer.jsx";
@@ -16,7 +15,6 @@ const Home = () => {
       <Proyect></Proyect>
       <Technology></Technology>
       <Footer></Footer>
-      <Status></Status>
     </>
   );
 };
