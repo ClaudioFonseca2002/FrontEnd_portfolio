af726daa2e564c3a89a808e849b6511a8cca1a9b
Fonseca Claudio <fonsecaclaudioroman2000@gmail.com>
2024-08-25 19:08:56 -0300

    Cambios en Home.jsx

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,12 +1,43 @@
 import "./Home.css";
 import Header from "../../components/header/Header";
-import Profile from "../../components/profile/Profile.jsx"
-import Proyect from "../../components/proyect/Proyect.jsx"
+import Profile from "../../components/profile/Profile.jsx";
+import Proyect from "../../components/proyect/Proyect.jsx";
+import { jwtDecode } from "jwt-decode";
 import Education from "../../components/education/Education.jsx";
 import Technology from "../../components/technology/Technology.jsx";
 import Footer from "../../components/footer/Footer.jsx";
+import { useAuth } from "../../utils/AuthContext.jsx";
 
 const Home = () => {
+  //Contexto
+  const { logout } = useAuth();
+
+  useEffect(() => {
+    return () => {
+      if (localStorage.getItem("token")) {
+        //Guardo el token en un estado
+        const storedToken = localStorage.getItem("token");
+        setToken(storedToken);
+
+        // Decodificar el token para obtener la fecha de expiración
+        const decodedToken = jwtDecode(storedToken);
+        const expirationTime = decodedToken.exp * 1000; // Convertir a milisegundos
+
+        // Calcular cuánto tiempo queda antes de que expire el token
+        const currentTime = new Date().getTime();
+        const timeUntilExpiration = (expirationTime - currentTime) / 1000;
+
+        if (currentTime > expirationTime) {
+          // El token ha expirado, eliminarlo del almacenamiento
+          localStorage.removeItem("token");
+          logout();
+        } else {
+          console.log("Token válido.");
+        }
+      }
+    };
+  }, [userLogged]); // Arreglo de dependencias vacío: se ejecuta solo una vez
+
   return (
     <>
       <Header></Header>
